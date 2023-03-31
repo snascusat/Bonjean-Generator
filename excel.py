acb246f64b6973c7cc00edcbd57c2ee013762a9e
mhdseby <mhdseby>
2023-04-01 02:16:58 +0530

    started out with workbook

--- excel.py
+++ excel.py
@@ -1,9 +1,14 @@
-from openpyxl import workbook
-from openpyxl import load_workbook
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Font, Color, PatternFill
+from smGenerator import smgenerator
 
+depthOfVessel = 20
+
+# EXTRACTING VALUES FROM WORKSHEET
 wb = load_workbook('sample offset table.xlsx')
 worksheet = wb.active
 stations = []
+waterlines = []
 offsetTable = []
 
 for i, row in enumerate(worksheet['A'][1:]):
@@ -12,8 +17,98 @@ for i, row in enumerate(worksheet['A'][1:]):
     for column in worksheet[i+2][1:]:
         offsetTable[i].append(column.value)
 
-print(stations)
-print(offsetTable)
+for column in worksheet[1][1:]:
+    waterlines.append(column.value)
 
 wb.close()
 
+wtlnSpacing = depthOfVessel / waterlines[-1]
+sm = smgenerator(waterlines)
+
+
+
+# FOR CUMULATIVE AREA
+smArea = []
+for n, station in enumerate(offsetTable):
+    i = 0
+    smArea.append([])
+    for multipliers in sm:
+        area = 0
+        for each in multipliers:
+            area += each * station[i]
+            i += 1
+        smArea[n].append(area)
+        i -= 1
+
+cumulativeArea = []
+for i, stationAreas in enumerate(smArea):
+    cumulativeArea.append([])
+    sum = 0
+    for each in stationAreas:
+        sum += (1/3) * wtlnSpacing * each
+        cumulativeArea[i].append(sum)
+
+
+
+
+# FOR CUMULATIVE MOMENT 
+lever = []
+for i in waterlines:
+    lever.append(i * wtlnSpacing)
+
+smMoment = []
+for n, station in enumerate(offsetTable):
+    i = 0
+    smMoment.append([])
+    for multipliers in sm:
+        moment = 0
+        for each in multipliers:
+            moment += each * station[i] * lever[i]
+            # print(each, lever[i], station[i])
+            i += 1
+        smMoment[n].append(moment)
+        i -= 1
+
+cumulativeMoment = []
+for i, stationMoments in enumerate(smMoment):
+    cumulativeMoment.append([])
+    sum = 0
+    for each in stationMoments:
+        sum += (1/3) * wtlnSpacing * each
+        cumulativeMoment[i].append(sum)
+
+
+
+# CREATING NEW WORKBOOK 
+wb = Workbook()
+template = wb.active
+
+header = ["STATION",0]
+title = ["WL",	"d",	"ΣfAs",	"As",	"ΣfM𝘇",	"M𝘇",	"Z"]
+template.append(header)
+template.append(title)
+
+for i, value in enumerate(waterlines):
+    template.cell(row=i+3, column=1, value=value)
+    template.cell(row=i+3, column=2, value=value * wtlnSpacing)
+
+
+
+for i in range(1,3):
+    for cell in template[i]:
+        cell.font = Font(bold=True, color="00FFFFFF")
+        cell.fill = PatternFill("solid", fgColor="00003366")
+
+for cell in template['A']:
+    cell.font = Font(bold=True, color="00FFFFFF")
+    cell.fill = PatternFill("solid", fgColor="00003366")
+
+
+#Adding values
+# for i, lst in enumerate(list_of_lists):
+#     worksheet = workbook.create_sheet(title=f"Sheet {i+1}")
+    
+#     for j, value in enumerate(lst):
+#         worksheet.cell(row=j+1, column=1, value=value)
+
+wb.save("sam.xlsx")
--- smGenerator.py
+++ smGenerator.py
@@ -1,19 +1,19 @@
-def smgenerator(stn):
+def smgenerator(ordinates):
     sm = []
-    if (len(stn)-1) %2 == 0:
-        r = int((len(stn)-1)/2)
+    if (len(ordinates)-1) %2 == 0:
+        r = int((len(ordinates)-1)/2)
         for i in range(r):
                 sm.append([1, 4, 1])
 
     
-    elif (len(stn)-1) %3 == 0:
-        r = int((len(stn)-1)/3)
+    elif (len(ordinates)-1) %3 == 0:
+        r = int((len(ordinates)-1)/3)
         for i in range(r):
             sm.append([1,3,3,1])
 
     else:
-         stn = stn[0:-2]
-         sm = smgenerator(stn)
+         ordinates = ordinates[0:-2]
+         sm = smgenerator(ordinates)
          sm.append([1, 4, 1])
 
     index = {}
@@ -24,12 +24,12 @@ def smgenerator(stn):
               index[ix] = i
               ix += 1
 
-    for i, ele in enumerate(stn):
-         if ele == 0 or i == (len(stn)-1):
+    for i, ele in enumerate(ordinates):
+         if ele == 0 or i == (len(ordinates)-1):
               continue
          pos = index[i]
          if ele<1 or ele % int(ele) != 0:
-              if stn[i+1] % int(stn[i+1]):                  
+              if ordinates[i+1] % int(ordinates[i+1]):                  
                    if len(sm[pos]) == 4:
                         sm[pos] = [0.5, 1.5, 1.5, 0.5]
                    else:
@@ -57,8 +57,8 @@ def smgenerator(stn):
     return sm
 
 
-stn = [0, 0.5,  1, 1.5,  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18.5, 19, 19.5, 20]
+# ordinates = [0, 0.5,  1, 1.5,  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18.5, 19, 19.5, 20]
 
-print(smgenerator(stn))
-print(len(stn))
+# print(smgenerator(ordinates))
+# print(len(ordinates))
 
